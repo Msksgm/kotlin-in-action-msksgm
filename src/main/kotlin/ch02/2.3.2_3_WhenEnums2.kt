cc88d7e2928a352b7ceef0527b70676c0851bab5
Msksgm <msksgm3124@gmail.com>
2022-08-01 08:36:04 +0900

    feat: pp.38 リスト2.14 修飾子なしでの enum 定数の参照

--- src/main/kotlin/ch02/2.3.2_3_WhenEnums2.kt
+++ src/main/kotlin/ch02/2.3.2_3_WhenEnums2.kt
@@ -4,9 +4,9 @@ import ch02.colors.Color
 import ch02.colors.Color.*
 
 fun getWarmth(color: Color) = when (color) {
-    Color.RED, Color.ORANGE, Color.YELLOW -> "warm"
-    Color.GREEN -> "neutral"
-    Color.BLUE, Color.INDIGO, Color.VIOLET -> "cold"
+    RED, ORANGE, YELLOW -> "warm"
+    GREEN -> "neutral"
+    BLUE, INDIGO, VIOLET -> "cold"
 }
 
 fun main(args: Array<String>) {
