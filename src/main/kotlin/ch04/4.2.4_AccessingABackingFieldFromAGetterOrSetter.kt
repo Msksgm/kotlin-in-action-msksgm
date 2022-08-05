024d05ac8a97f03bc2175e6354c4b01a039e6075
Msksgm <msksgm3124@gmail.com>
2022-08-06 07:38:57 +0900

    feat: p.112 リスト 4.15

---
+++ src/main/kotlin/ch04/4.2.4_AccessingABackingFieldFromAGetterOrSetter.kt
@@ -0,0 +1,22 @@
+package ch04.ex2_4_AccessingABackingFieldFromAGetterOrSetter
+
+class User(val name: String) {
+    var address: String = "unspecified"
+        set(value: String) {
+            println(
+                """
+                Address was changed for $name:
+                "$field" -> "$value"
+                """.trimIndent()
+            )
+            field = value
+        }
+}
+
+fun main(args: Array<String>) {
+    val user = User("Alice")
+    println(user.address)
+    user.address = "Elsenheimerstrasse 47, 80687 Muenchen"
+    println(user.address)
+}
+
