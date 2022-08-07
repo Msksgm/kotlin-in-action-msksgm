36df35894ba38573bf8f575bb245f39179b14db3
Msksgm <msksgm3124@gmail.com>
2022-08-07 11:15:52 +0900

    feat: pp.150--pp.151

---
+++ src/main/kotlin/ch05/5.2.1_4_FilterMap3.kt
@@ -0,0 +1,10 @@
+package ch05.ex2_1_4_FilterMap3
+
+data class Person(val name: String, val age: Int)
+
+fun main(args: Array<String>) {
+    val people = listOf(Person("Alice", 29), Person("Bob", 31))
+    println(people.map { it.name })
+    println(people.map(Person::name))
+    println(people.filter { it.age > 30 }.map(Person::name))
+}
